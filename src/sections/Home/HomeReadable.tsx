1ac7a6693ba68cb6cc6512d78906f2569d510963
me <testamirali80@yahoo.com>
2022-07-10 13:13:45 +0430

    Home Redable & Footer Button Updated

--- src/sections/Home/HomeReadable.tsx
+++ src/sections/Home/HomeReadable.tsx
@@ -8,16 +8,20 @@ const HomeReadable = () => {
     <section className="Home_Readable border mt-3 mb-5 px-3 py-4">
       {HomeRedableData.map((read, index) => (
         <div key={index}>
-          {read.title.length > 0 && <h2 className="fw-bolder mb-3">{read.title}</h2>}
+          {read.title.length > 0 && (
+            <h2 className="fw-bolder mb-3 CDEFAULT">{read.title}</h2>
+          )}
           {read.titleBlue.length > 0 && (
-            <h3 className="fw-bolder text-primary mb-3">{read.titleBlue}</h3>
+            <h3 className="fw-bolder text-primary mb-3 CURSOR">
+              {read.titleBlue}
+            </h3>
           )}
           {read.pictureOne.length > 0 && (
             <div className="Home_Readable_PictureOneBox d-flex justify-content-center my-5">
               <img src={read.pictureOne} alt="Pic..." className="" />
             </div>
           )}
-          <p className="Home_Readable_lorem text-muted lead">
+          <p className="Home_Readable_lorem text-muted lead CDEFAULT">
             از آنجا که لورم ایپسوم، شباهت زیادی به متن های واقعی دارد، طراحان
             معمولا از لورم ایپسوم استفاده میکنند تا فقط به مشتری یا کار فرما
             نشان دهند که قالب طراحی شده بعد از اینکه متن در آن قرار میگرد چگونه
